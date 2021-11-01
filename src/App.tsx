14ae8183ac8b0c2379bea6c66c0cb84e9d247ff8
Tamas Hoffman <tms.hoffman@gmail.com>
2021-11-01 21:47:50 +0100

    Update: Updated dependencies + text color change

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
         </p>
         <p className="divide-x divide-gray-50">
           <a
-            className="px-4 text-yellow-10 hover:underline"
+            className="px-4 text-purple-300 hover:underline"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
@@ -27,7 +27,7 @@ function App() {
           </a>
 
           <a
-            className="px-4 text-yellow-10 hover:underline"
+            className="px-4 text-purple-300 hover:underline"
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"
@@ -36,7 +36,7 @@ function App() {
           </a>
 
           <a
-            className="px-4 text-yellow-10 hover:underline"
+            className="px-4 text-purple-300 hover:underline"
             href="https://tailwindcss.com/docs"
             target="_blank"
             rel="noopener noreferrer"
